b78366a516e49f8ba1cd4d4fa71372cc8a908a62
Anton Solianyk <solyanicks@gmail.com>
2025-08-05 02:29:19 +0200

    refactor: add ErrorBodyFactory

--- backend/src/main/java/io/github/antonio/backnotfront/ratelimiter/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/io/github/antonio/backnotfront/ratelimiter/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package io.github.antonio.backnotfront.ratelimiter.handler;
 
 import io.github.antonio.backnotfront.ratelimiter.exception.ApplicationException;
+import io.github.antonio.backnotfront.ratelimiter.utility.ErrorBodyFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -20,22 +21,15 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<?> handleException(ApplicationException exception) {
-        Map<String, Object> body = createBody(exception.getStatusCode(), exception.getMessage());
+        Map<String, Object> body = ErrorBodyFactory.generateBody(exception.getMessage(), exception.getStatusCode());
         return new ResponseEntity<>(body, HttpStatus.valueOf(exception.getStatusCode()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception exception) {
         logger.error(Arrays.toString(exception.getStackTrace()));
-        Map<String, Object> body = createBody(500, "Internal Server Error.");
+        Map<String, Object> body = ErrorBodyFactory.generateBody("Internal Server Error.", 500);
         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    private Map<String, Object> createBody(int statusCode, String errorMessage) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", statusCode);
-        body.put("error", errorMessage);
-        return body;
-    }
 }
---
+++ backend/src/main/java/io/github/antonio/backnotfront/ratelimiter/utility/ErrorBodyFactory.java
@@ -0,0 +1,17 @@
+package io.github.antonio.backnotfront.ratelimiter.utility;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ErrorBodyFactory {
+
+    public static Map<String, Object> generateBody(String error, Integer status){
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", status);
+        body.put("error", error);
+        body.put("timestamp", System.currentTimeMillis());
+        return body;
+    }
+
+}
