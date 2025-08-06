2cbfff9d23540ce7c49c9f7f62d86461001a32dc
Anton Solianyk <solyanicks@gmail.com>
2025-08-06 18:26:04 +0200

    feat: add user caching for login endpoint and modify RedisConfig to enable multiple cache configs in the future

--- backend/src/main/java/io/github/antonio/backnotfront/ratelimiter/config/RedisConfig.java
+++ backend/src/main/java/io/github/antonio/backnotfront/ratelimiter/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package io.github.antonio.backnotfront.ratelimiter.config;
 
+import io.github.antonio.backnotfront.ratelimiter.dto.response.LoginResponseDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -11,19 +12,27 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+        RedisCacheConfiguration loginResponseCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .disableCachingNullValues()
+                .entryTtl(Duration.ofHours(2))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
-                        new Jackson2JsonRedisSerializer<>(Integer.class)
+                        new Jackson2JsonRedisSerializer<>(LoginResponseDto.class)
                 ));
 
+        Map<String, RedisCacheConfiguration> configMap = new HashMap<>();
+        configMap.put("LOGIN_RESPONSES", loginResponseCacheConfiguration);
+
         return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(cacheConfiguration)
+                .withInitialCacheConfigurations(configMap)
                 .build();
     }
 
--- backend/src/main/java/io/github/antonio/backnotfront/ratelimiter/service/impl/UserServiceImpl.java
+++ backend/src/main/java/io/github/antonio/backnotfront/ratelimiter/service/impl/UserServiceImpl.java
@@ -6,9 +6,12 @@ import io.github.antonio.backnotfront.ratelimiter.dto.response.LoginResponseDto;
 import io.github.antonio.backnotfront.ratelimiter.dto.response.RegisterResponseDto;
 import io.github.antonio.backnotfront.ratelimiter.exception.UnauthorizedException;
 import io.github.antonio.backnotfront.ratelimiter.model.User;
+import io.github.antonio.backnotfront.ratelimiter.model.UserPrincipal;
 import io.github.antonio.backnotfront.ratelimiter.repository.impl.UserRepository;
 import io.github.antonio.backnotfront.ratelimiter.service.UserService;
 import io.github.antonio.backnotfront.ratelimiter.utility.JwtService;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,23 +26,27 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final CacheManager cacheManager;
 
-    public UserServiceImpl(UserRepository repository, PasswordEncoder passwordEncoder, JwtService jwtService, UserDetailsService userDetailsService) {
+    public UserServiceImpl(UserRepository repository, PasswordEncoder passwordEncoder, JwtService jwtService, UserDetailsService userDetailsService, CacheManager cacheManager) {
         this.repository = repository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
+        this.cacheManager = cacheManager;
     }
 
     @Override
+    @Cacheable(value = "LOGIN_RESPONSES", key = "#requestDto")
     public LoginResponseDto login(LoginRequestDto requestDto) {
+        User foundUser;
         Optional<User> userOptional = repository.findByEmail(requestDto.email());
         if (userOptional.isEmpty()) rejectLoginRequest();
-        User user = userOptional.get();
-        if (!passwordEncoder.matches(requestDto.password(), user.getPassword())) {
+        foundUser = userOptional.get();
+        if (!passwordEncoder.matches(requestDto.password(), foundUser.getPassword())) {
             rejectLoginRequest();
         }
-        UserDetails userDetails = userDetailsService.loadUserByUsername(user.getEmail());
+        UserDetails userDetails = new UserPrincipal(foundUser);
         String accessToken = jwtService.generateAccessToken(userDetails);
         String refreshToken = jwtService.generateRefreshToken(userDetails);
         return new LoginResponseDto(accessToken, refreshToken);
