fc23e92860d73442b51ca82aae3fc2ea6de38c91
Anton Solianyk <solyanicks@gmail.com>
2025-08-28 22:31:14 +0200

    refactor: improve readability and maintanability of PolicyValidatorTest

--- backend/src/test/java/io/github/antonio/backnotfront/ratelimiter/utility/PolicyValidatorTest.java
+++ backend/src/test/java/io/github/antonio/backnotfront/ratelimiter/utility/PolicyValidatorTest.java
@@ -1,78 +1,124 @@
 package io.github.antonio.backnotfront.ratelimiter.utility;
 
+import io.github.antonio.backnotfront.ratelimiter.exception.BadRequestException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//@DataJpaTest
+
+@DisplayName("Test validation methods in PolicyValidator class")
 public class PolicyValidatorTest {
 
 
+    @DisplayName("too big capacity => false")
     @Test
-    public void validateCapacity_TooBigCapacity_False(){
+    public void testValidateCapacity_WhenTooBigCapacity_ShouldReturnFalse() {
         // arrange
+        int capacity = 99_999;
+        Boolean expectedResult = false;
+
         // act
-        Boolean val = PolicyValidator.validateCapacity(99_999);
+        Boolean val = PolicyValidator.validateCapacity(capacity);
+
         // assert
-        Assertions.assertEquals(val, false);
+        Assertions.assertEquals(val, expectedResult);
     }
+
+    @DisplayName("too small capacity => false")
     @Test
-    public void validateCapacity_TooSmallCapacity_False(){
+    public void testValidateCapacity_WhenTooSmallCapacity_ShouldReturnFalse() {
         // arrange
+        int capacity = 0;
+        Boolean expectedResult = false;
+
         // act
-        Boolean val = PolicyValidator.validateCapacity(0);
+        Boolean val = PolicyValidator.validateCapacity(capacity);
+
         // assert
-        Assertions.assertEquals(val, false);
+        Assertions.assertEquals(val, expectedResult);
     }
+
+    @DisplayName("normal capacity => true")
     @Test
-    public void validateCapacity_NormalCapacity_True(){
+    public void testValidateCapacity_WhenNormalCapacity_ShouldReturnTrue() {
         // arrange
+        int capacity = 90;
+        Boolean expectedResult = true;
+
         // act
-        Boolean val = PolicyValidator.validateCapacity(90);
+        Boolean val = PolicyValidator.validateCapacity(capacity);
+
         // assert
-        Assertions.assertEquals(val, true);
+        Assertions.assertEquals(val, expectedResult);
     }
+
+    @DisplayName("too small window size => true")
     @Test
-    public void validateWindowSize_ToSmallWindowSize_False(){
+    public void testValidateWindowSize_WhenToSmallWindowSize_ShouldReturnFalse() {
         // arrange
+        int windowSize = 2;
+        Boolean expectedResult = false;
+
         // act
-        Boolean val = PolicyValidator.validateWindowSize(2);
+        Boolean val = PolicyValidator.validateWindowSize(windowSize);
+
         // assert
-        Assertions.assertEquals(val, false);
+        Assertions.assertEquals(val, expectedResult);
     }
+
+    @DisplayName("too large window size => false")
     @Test
-    public void validateWindowSize_ToBigWindowSize_False(){
+    public void testValidateWindowSize_WhenToBigWindowSize_ShouldReturnFalse() {
         // arrange
+        int windowSize = 200_000;
+        Boolean expectedResult = false;
+
         // act
-        Boolean val = PolicyValidator.validateWindowSize(200_000);
+        Boolean val = PolicyValidator.validateWindowSize(windowSize);
+
         // assert
-        Assertions.assertEquals(val, false);
+        Assertions.assertEquals(val, expectedResult);
     }
+
+    @DisplayName("normal window size => true")
     @Test
-    public void validateWindowSize_NormalWindowSize_True(){
+    public void testValidateWindowSize_WhenNormalWindowSize_ShouldReturnTrue() {
         // arrange
+        int windowSize = 60;
+        Boolean expectedResult = true;
+
         // act
-        Boolean val = PolicyValidator.validateWindowSize(60);
+        Boolean val = PolicyValidator.validateWindowSize(windowSize);
+
         // assert
-        Assertions.assertEquals(val, true);
+        Assertions.assertEquals(val, expectedResult);
     }
+
+    @DisplayName("too many policies => false")
     @Test
-    public void validatePolicyNumber_TooManyPolicies_False(){
+    public void testValidatePolicyNumber_WhenTooManyPolicies_ShouldReturnFalse() {
         // arrange
+        int policyNumber = 991;
+        Boolean expectedResult = false;
+
         // act
-        Boolean val = PolicyValidator.validatePolicyNumber(991);
+        Boolean val = PolicyValidator.validatePolicyNumber(policyNumber);
+
         // assert
-        Assertions.assertEquals(val, false);
+        Assertions.assertEquals(val, expectedResult);
     }
+
+    @DisplayName("normal number of policies => true")
     @Test
-    public void validatePolicyNumber_NormalNumberOfPolicies_True(){
+    public void testValidatePolicyNumber_WhenNormalNumberOfPolicies_ShouldReturnTrue() {
         // arrange
+        int policyNumber = 6;
+        Boolean expectedResult = true;
+
         // act
-        Boolean val = PolicyValidator.validatePolicyNumber(6);
+        Boolean val = PolicyValidator.validatePolicyNumber(policyNumber);
+
         // assert
-        Assertions.assertEquals(val, true);
+        Assertions.assertEquals(val, expectedResult);
     }
 }
